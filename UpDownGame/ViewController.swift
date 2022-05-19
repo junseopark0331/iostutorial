a0df9c999d7fdb2e90f3877e2ab8a09ff5755662
junseopark <junseopark0331@icloud.com>
2022-05-19 23:35:25 +0900

    버튼으로 숫자를 조절하여 자신이 고른 값이 몇인지 나오게끔 했다. 리셋버튼을 누르면 초기화가 되게끔 했다. 큰 크기의 글씨를 써봤다

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+        print(sender.value)
+        
+    }
+    @IBAction func touchUpHitButton(_ sender: UIButton){
+        print(slider.value)
+    }
+    @IBAction func touchUpResetButton(_ sender: UISlider){
+        print("touch up reset button")
+    }
 }
 
